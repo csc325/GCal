4b7ce2273b0879c40731855a1b5dd2f561fd3a9d
Jing Tao Liu <liujingt@cs.grinnell.edu>
2010-11-22 13:31:55 -0600

    got rid of some unused files

--- GCal/mysql_connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $connect = mysql_connect('localhost', 'csc325generic', 'password');
-    mysql_select_db('CSC325');
-?>
--- footer.php
+++
@@ -1,6 +0,0 @@
-    <div class="foot">
-        <div class="col large">CSC325 Databases and Web Application</div>
-        <div class="col small">Blah Blah Legal Stuff Blah</div>
-    </div>
-</div>
-</div>
