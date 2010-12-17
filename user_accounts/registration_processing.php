ec35706484306d951e5e3b3a24e58c28d4bbdc99
Andrew Hirakawa <akhirakawa@gmail.com>
2010-12-17 14:02:04 -0600

    adheres to PEAR

--- user_accounts/registration_processing.php
+++ user_accounts/registration_processing.php
@@ -1,26 +1,54 @@
 <?php
-    require_once '../global.php';
-    require_once 'functions.php';
+/*
+* registration_processing: script called to email user and insert user data
+* into database
+* PHP version 5
+*
+* LICENSE: This source file is subject to version 3.01 of the PHP license
+* that is available through the world-wide-web at the following URI:
+* http://www.php.net/license/3_01.txt. If you did not receive a copy of
+* the PHP License and are unable to obtain it through the web, please
+* send a note to license@php.net so we can mail you a copy immediately.
+*
+* @category user functions
+* @author CSC-325 Database and Web Application Fall 2010 Class
+* @license http://www.php.net/license/3_01.txt PHP License 3.01
+* @version 3.0
+*/
+require_once '../global.php';
+require_once 'functions.php';
 
-    // Sanitize input
-    $username  = addslashes($_POST['username']);
-    $password1  = addslashes($_POST['password1']);
-    $password2 = addslashes($_POST['password2']);
-    $user_exists = user_exists($_POST['username']);
+// Sanitize input
+$username    = addslashes($_POST['username']);
+$password1   = addslashes($_POST['password1']);
+$password2   = addslashes($_POST['password2']);
+$user_exists = user_exists($_POST['username']);
 
-    // Redirect to registration form if something is wrong
-    $redirect = 'Location: http://www.cs.grinnell.edu'.ed(false).'user_accounts/registration_form.php';
-    if ($user_exists) { header($redirect.'?q=exists&user='.$_POST['username']); exit(); }
-    if ($password1 != $password2) { header($redirect.'?q=nomatch'); exit(); }
-    if (preg_match('/[^a-zA-Z0-9]/',$username)) { header($redirect.'?q=chars'); exit(); }
+// Redirect to registration form if something is wrong
+$redirect = 'Location: http://www.cs.grinnell.edu'.ed(false)
+  .'user_accounts/registration_form.php';
+if ($user_exists) { 
+  header($redirect.'?q=exists&user='.$_POST['username']); 
+  exit(); 
+}
+
+if ($password1 != $password2) { 
+  header($redirect.'?q=nomatch'); 
+  exit(); 
+}
+
+if (preg_match('/[^a-zA-Z0-9]/',$username)) { 
+  header($redirect.'?q=chars'); 
+  exit(); 
+}
     
 
-    // If all checks are cleared, continue registration
-    $encoded = md5($_POST['password1']);
-    $regNo = randomPasswordGen(false);
+// If all checks are cleared, continue registration
+$encoded = md5($_POST['password1']);
+$regNo = randomPasswordGen(false);
     
-    // Message
-    $message = "<html><body>
+// Message
+$message = "<html><body>
                 <p>Welcome to Grinnell Open Calender! <br /><br />
                 To activate your account, <a href='".ed(false)."user_accounts/validate.php?conf=$regNo'>click here</a>
                 or copy and paste the code below into the<br>validation form: 
@@ -30,21 +58,21 @@
 	            Your activation code is: $regNo<p></p>
                 </body></html>";
 
-    // Headers
-    $header = 'MIME-Version: 1.0' . "\r\n" .
-              'Content-type: text/html; charset=iso-8859-1' . "\r\n" .
-              'From: Grinnell Open Calendar <webmaster@grinnellopencalender.com>' . "\r\n" .
-              'Reply-To: webmaster@grinnellopencalender.com' . "\r\n" .
-              'X-Mailer: PHP/' . phpversion();
+// Headers
+$header = 'MIME-Version: 1.0' . "\r\n" .
+  'Content-type: text/html; charset=iso-8859-1' . "\r\n" .
+  'From: Grinnell Open Calendar <webmaster@grinnellopencalender.com>' . "\r\n" .
+  'Reply-To: webmaster@grinnellopencalender.com' . "\r\n" .
+  'X-Mailer: PHP/' . phpversion();
 
-    // Send Message
-    $sent = mail( $username.'@grinnell.edu', 'Account Activation', $message, $header);
+// Send Message
+$sent = mail( $username.'@grinnell.edu', 'Account Activation', $message, $header);
     
-    // Insert user into database
-    $query = "INSERT INTO users (displayName, email, password, confirmed)
+// Insert user into database
+$query = "INSERT INTO users (displayName, email, password, confirmed)
 	          VALUES ('$username', '$username@grinnell.edu', '$encoded', $regNo);";
     
-    $result = mysql_query($query);
+$result = mysql_query($query);
     
-    header('Location: '.ed(false).'user_accounts/validate.php');
+header('Location: '.ed(false).'user_accounts/validate.php');
 ?>
--- user_accounts/validate.php
+++ user_accounts/validate.php
@@ -1,70 +1,89 @@
 <?php
-    require_once '../global.php';
-    require_once 'functions.php';
+/*
+* validate.php provides validation to newly created users
+* PHP version 5
+*
+* LICENSE: This source file is subject to version 3.01 of the PHP license
+* that is available through the world-wide-web at the following URI:
+* http://www.php.net/license/3_01.txt. If you did not receive a copy of
+* the PHP License and are unable to obtain it through the web, please
+* send a note to license@php.net so we can mail you a copy immediately.
+*
+* @category user functions
+* @author CSC-325 Database and Web Application Fall 2010 Class
+* @license http://www.php.net/license/3_01.txt PHP License 3.01
+* @version 3.0
+*/
+require_once '../global.php';
+require_once 'functions.php';
 
-    if (isset($_GET['conf'])) {
-        $conf_num = addslashes($_GET['conf']);
-        $query = "SELECT * FROM users WHERE users.confirmed = $conf_num;";
-        $result = mysql_query($query);
+if (isset($_GET['conf'])) {
+  $conf_num = addslashes($_GET['conf']);
+  $query = "SELECT * 
+            FROM users 
+            WHERE users.confirmed = $conf_num;";
+  $result = mysql_query($query);
         
-        if($result) {
-            $row = mysql_fetch_array($result,MYSQL_ASSOC);
+  if($result) {
+    $row = mysql_fetch_array($result,MYSQL_ASSOC);
             
-            if($row[confirmed] == $conf_num) {
-                $query = "UPDATE users SET confirmed = null WHERE confirmed = $conf_num";
-                $result = mysql_query($query);
+    if($row[confirmed] == $conf_num) {
+      $query = "UPDATE users 
+                SET confirmed = null 
+                WHERE confirmed = $conf_num";
+      $result = mysql_query($query);
                 
-                $_POST['username'] = $row[displayName];
-                $_POST['password'] = $row[password];
-                $_POST['static'] = 'true';
-                require_once 'login_processing.php';
+      $_POST['username'] = $row[displayName];
+      $_POST['password'] = $row[password];
+      $_POST['static'] = 'true';
+      require_once 'login_processing.php';
             
-                require_once '../header.php';
-                echo "<div class = 'body'>";
-                echo "<div class = 'col large'>";
+      require_once '../header.php';
+      echo "<div class = 'body'>";
+      echo "<div class = 'col large'>";
                 
-                echo "<h1 class='head'>Welcome $row[displayName]!</h1>";
-                echo '<p>Your account has been successfully activated! Now 
+      echo "<h1 class='head'>Welcome $row[displayName]!</h1>";
+      echo '<p>Your account has been successfully activated! Now 
                       you\'ll be able to <a href="'.ed(false).'forms.php">add 
                       events</a> for all to see, and view private Grinnell 
                       only events!</p>';
-                echo '<p>As always, remember that SELF GOV IS LOVE.</p>';
-            } else {
-                require_once '../header.php';
-                echo "<div class = 'body'>";
-                echo "<div class = 'col large'>";
-                
-                echo "<h1 class='head'>Wrong confirmation number entered</h1>
-                      <p><a href='validate.php'>Please try again</a></p>";
-            }
-        }
+      echo '<p>As always, remember that SELF GOV IS LOVE.</p>';
     } else {
-        require_once '../header.php';
-        echo "<div class = 'body'>";
-        echo "<div class = 'col large'>";
-        ?>
-        
-        <h1 class="head">Account Validation</h1>
-        <p>Congratulations you have been registered!  However, in order to
-        activate your account you need to enter the validation code that has 
-        been sent to your Grinnell email account into the form below.</p>
-        
-        <form action="validate.php" method="get">
-            <div class="form-unit long">
-                <label for="conf">Validation Code:</label>
-                <input type="text" name="conf" id="conf">
-            </div>
-            <div class="form-unit long">
-                <label>&nbsp;</label>
-                <input type="submit" value="Validate">
-            </div>
-        </form>
-        
-        <?php
+      require_once '../header.php';
+      echo "<div class = 'body'>";
+      echo "<div class = 'col large'>";
+                
+      echo "<h1 class='head'>Wrong confirmation number entered</h1>
+                      <p><a href='validate.php'>Please try again</a></p>";
     }
+  }
+} else {
+  require_once '../header.php';
+  echo "<div class = 'body'>";
+  echo "<div class = 'col large'>";
+  ?>
+        
+  <h1 class="head">Account Validation</h1>
+     <p>Congratulations you have been registered!  However, in order to
+     activate your account you need to enter the validation code that has 
+     been sent to your Grinnell email account into the form below.</p>
+        
+     <form action="validate.php" method="get">
+     <div class="form-unit long">
+     <label for="conf">Validation Code:</label>
+                         <input type="text" name="conf" id="conf">
+                         </div>
+                         <div class="form-unit long">
+                         <label>&nbsp;</label>
+                                          <input type="submit" value="Validate">
+                                          </div>
+                                          </form>
+        
+                                          <?php
+                                          }
     
-    echo "</div>";    
-    require_once '../sidebar.php';
-    echo "</div>";
-    require_once '../footer.php'; 
+echo "</div>";    
+require_once '../sidebar.php';
+echo "</div>";
+require_once '../footer.php'; 
 ?>
