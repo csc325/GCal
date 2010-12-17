87e44359da0d035e417e166e865396fd2c0e5e32
Andrew Hirakawa <akhirakawa@gmail.com>
2010-12-17 13:40:56 -0600

    adheres to PEAR

--- user_accounts/confirmation_processing.php
+++ user_accounts/confirmation_processing.php
@@ -1,28 +1,48 @@
 <?php
-    include '../functions/connection.php';
-    include '../global.php';
-    include '../header.php';
-    include 'functions.php';
+/*
+* confirmation_processing.php
+* PHP version 5
+*
+* LICENSE: This source file is subject to version 3.01 of the PHP license
+* that is available through the world-wide-web at the following URI:
+* http://www.php.net/license/3_01.txt. If you did not receive a copy of
+* the PHP License and are unable to obtain it through the web, please
+* send a note to license@php.net so we can mail you a copy immediately.
+*
+* @category processing
+* @author CSC-325 Database and Web Application Fall 2010 Class
+* @license http://www.php.net/license/3_01.txt PHP License 3.01
+* @version 3.0
+*/
+include '../functions/connection.php';
+include '../global.php';
+include '../header.php';
+include 'functions.php';
 
-    echo "<div class = 'body'>";
-    echo "<div class = 'col large'>";
+echo "<div class = 'body'>";
+echo "<div class = 'col large'>";
       
-    $conf_num = mysql_real_escape_string($_POST["confirm"]);
-    $conf_num = str_replace(" ","", $conf_num);
-    $query = "SELECT confirmed, displayName FROM users WHERE users.confirmed = $conf_num;";
-    $result = mysql_query($query);
-    if(!have_error($result, $query)) {
-        $row = mysql_fetch_row($result);
-        if($row[0] == $conf_num) {
-            echo "Registration for $row[1] confirmed.";
-            $result = mysql_query("UPDATE users SET confirmed = null WHERE confirmed = $conf_num;");
-        } else {
-            echo "Wrong confirmation number entered.";
-        }
-    }
+$conf_num = mysql_real_escape_string($_POST["confirm"]);
+$conf_num = str_replace(" ","", $conf_num);
+$query = "SELECT confirmed, displayName 
+          FROM users 
+          WHERE users.confirmed = $conf_num;";
+$result = mysql_query($query);
+
+if (!have_error($result, $query)) {
+  $row = mysql_fetch_row($result);
+  if ($row[0] == $conf_num) {
+    echo "Registration for $row[1] confirmed.";
+    $result = mysql_query("UPDATE users 
+                           SET confirmed = null 
+                           WHERE confirmed = $conf_num;");
+  } else {
+    echo "Wrong confirmation number entered.";
+  }
+}
     
-    echo "</div>";    
-    include '../sidebar.php';
-    echo "</div>";
-    include '../footer.php'; 
+echo "</div>";    
+include '../sidebar.php';
+echo "</div>";
+include '../footer.php'; 
 ?>
--- user_accounts/forgot_password.php
+++ user_accounts/forgot_password.php
@@ -1,10 +1,29 @@
 <?php
-    require_once '../global.php';
-    require_once '../header.php';
-    require_once 'functions.php';
+/*
+* forgot_password
+* PHP version 5
+*
+* LICENSE: This source file is subject to version 3.01 of the PHP license
+* that is available through the world-wide-web at the following URI:
+* http://www.php.net/license/3_01.txt. If you did not receive a copy of
+* the PHP License and are unable to obtain it through the web, please
+* send a note to license@php.net so we can mail you a copy immediately.
+*
+* @category user functions
+* @author CSC-325 Database and Web Application Fall 2010 Class
+* @license http://www.php.net/license/3_01.txt PHP License 3.01
+* @version 3.0
+*/
+require_once '../global.php';
+require_once '../header.php';
+require_once 'functions.php';
     
-    function reset_form () {
-        echo '<p>Enter your username below and a new password will be sent to your email. 
+/*generate reset password HTML form
+ *@return HTML form
+ */
+function reset_form () 
+{
+  echo '<p>Enter your username below and a new password will be sent to your email. 
         You can change your password by going to "Settings" after you\'ve logged in.</p>
         
         <form action="forgot_password.php" method="post">
@@ -12,28 +31,30 @@
             <input type="hidden" name="sent" value="1"/>
             <input type="submit" value="Send New Password" />
         </form>';
-    }
+}
 ?>
-    <div class="body">
-        <div class="col large">
-        <h1 class="head">Forgot Your Password?</h1>
-        <?php 
-            if($_POST['sent']) :
-                $query = "SELECT * FROM users WHERE displayName = '$_POST[displayName]'";
-                $result = mysql_query ($query);
-                $row = mysql_fetch_array($result);
+<div class="body">
+  <div class="col large">
+  <h1 class="head">Forgot Your Password?</h1>
+  <?php 
+  if($_POST['sent']) :
+    $query = "SELECT * 
+              FROM users 
+              WHERE displayName = '$_POST[displayName]'";
+  $result = mysql_query ($query);
+  $row = mysql_fetch_array($result);
                 
-                if (passwordReset($row[email])) {
-                    echo '<h3>A new password has been sent to your Grinnell e-mail address.</h3>';
-                } else {
-                    echo '<h3>Email address not recognized.</h3>';
-                    reset_form();
-                }
-            else :
-                reset_form();
-            endif;
-        ?>
-    </div>
+  if (passwordReset($row[email])) {
+    echo '<h3>A new password has been sent to your Grinnell e-mail address.</h3>';
+  } else {
+    echo '<h3>Email address not recognized.</h3>';
+    reset_form();
+  }
+  else :
+    reset_form();
+  endif;
+  ?>
+</div>
 <?php include '../sidebar.php'; ?>
 </div>
 <?php include '../footer.php'; ?>
