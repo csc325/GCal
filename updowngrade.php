aacadc124295278d321ef8d7d24cb6d014bde41c
Andrew Hirakawa <akhirakawa@gmail.com>
2010-12-17 14:36:15 -0600

    aheres to PEAR

--- updowngrade.php
+++ updowngrade.php
@@ -1,12 +1,27 @@
 <?php
+/*
+* updowngrade: script to change category from permanent to non permanent or
+* vice versa
+* PHP version 5
+*
+* LICENSE: This source file is subject to version 3.01 of the PHP license
+* that is available through the world-wide-web at the following URI:
+* http://www.php.net/license/3_01.txt. If you did not receive a copy of
+* the PHP License and are unable to obtain it through the web, please
+* send a note to license@php.net so we can mail you a copy immediately.
+*
+* @category admin feature
+* @author CSC-325 Database and Web Application Fall 2010 Class
+* @license http://www.php.net/license/3_01.txt PHP License 3.01
+* @version 3.0
+*/
 include 'functions/connection.php';
 if ( $_POST )
   {
     /* Change category permanence */
     $queryDelete = 'UPDATE categories SET permanent=0 WHERE categoryID = ';
-    $queryAdd = 'UPDATE categories SET permanent=1 WHERE categoryID = ';
-    
-    $addArray = array_keys( $_POST, 'upgrade' );
+    $queryAdd    = 'UPDATE categories SET permanent=1 WHERE categoryID = ';    
+    $addArray    = array_keys( $_POST, 'upgrade' );
     $deleteArray = array_keys( $_POST, 'downgrade' );
     
     if ( $addArray )
@@ -14,7 +29,8 @@ if ( $_POST )
         $queryAdd .= implode( ' OR categoryID = ', $addArray );    
         $resultAdd = mysql_query($queryAdd);
         /* To do: error check */
-        echo 1; exit();
+        echo 1; 
+        exit();
       }
     
     if ( $deleteArray )
@@ -22,7 +38,8 @@ if ( $_POST )
         $queryDelete .= implode( ' OR categoryID = ', $deleteArray ); 
         $resultDelete = mysql_query($queryDelete);
         /* To do: error check */
-        echo 1; exit();
+        echo 1; 
+        exit();
       }
   }
 ?>
